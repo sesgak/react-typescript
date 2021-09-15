7cb2be45cf7c26cd601a7f69709deb2691dcfe49
sesgak <seshigeruak@gmail.com>
2021-09-15 12:17:33 +0900

    第5章

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
 	const [loading, setLoading] = useState(false);
-	const [country, setCountry] = useState("");
+	const [country, setCountry] = useState("japan");
 	const [countryData, setCountryData] = useState({
 		date: "",
 		newCofirmed: "",
@@ -20,23 +20,26 @@ function App() {
     
 	const [allCountriesData, setAllCountriesData] = useState([]);
 
-    const getCountryData = () => {
-		setLoading(true);
-        fetch(`https://api.covid19api.com/country/${country}`)
-        .then(res => res.json())
-        .then(data => {
-			setCountryData({
-				date: data[data.length -2].Date,
-				newCofirmed:data[data.length -2].Confirmed - data[data.length -3].Confirmed,
-				totalCofirmed:data[data.length -2].Confirmed,
-				newDeaths:data[data.length -2].Deaths - data[data.length -3].Deaths,
-				totalDeaths:data[data.length -2].Deaths
-			});
-			setLoading(false);
-		})
-		.catch(err => alert("エラーが発生しました。ページをリロードしてもう一度トライしてください。"));
-	}
-	
+	useEffect(() => {
+		const getCountryData = () => {
+			setLoading(true);
+			fetch(`https://api.covid19api.com/country/${country}`)
+			.then(res => res.json())
+			.then(data => {
+				setCountryData({
+					date: data[data.length -2].Date,
+					newCofirmed:data[data.length -2].Confirmed - data[data.length -3].Confirmed,
+					totalCofirmed:data[data.length -2].Confirmed,
+					newDeaths:data[data.length -2].Deaths - data[data.length -3].Deaths,
+					totalDeaths:data[data.length -2].Deaths
+				});
+				setLoading(false);
+			})
+			.catch(err => alert("エラーが発生しました。ページをリロードしてもう一度トライしてください。"));
+		}
+		getCountryData();
+	},[country])
+
 	useEffect(() =>{
 		fetch("https://api.covid19api.com/summary")
 		.then(res => res.json())
@@ -49,8 +52,7 @@ function App() {
 			<Switch>
 				<Route exact path="/">
 					<TopPage countriesJson={countriesJson} setCountry={setCountry}
-					 getCountryData={getCountryData} countryData={countryData}
-					 loading={loading} />
+					 countryData={countryData} loading={loading} />
 				</Route>
 				<Route　exact path="/world">
 					<WorldPage allCountriesData={allCountriesData} />
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -6,13 +6,13 @@ import Selector from "../components/Selector";
 import Results from "../components/Results";
 
 const TopPage = ({countriesJson, setCountry,
-                  getCountryData, countryData, loading}) => {
+                  countryData, loading}) => {
     return (
         <div className="top-page-container">
             <div>
                 <Header />
                 <Title />
-                <Selector countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData}/>
+                <Selector countriesJson={countriesJson} setCountry={setCountry} />
                 <Results  countryData={countryData} loading={loading}/>
             </div>
         </div>
